27f879029029c55fcb278c6e6bb5f09a3f3490cc
Yash <yashsavani004@gmail.com>
2024-02-08 07:25:27 +0530

    Conn changed

--- Frontend/src/Graphql/conn.js
+++ Frontend/src/Graphql/conn.js
@@ -1,8 +1,21 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+// import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export const client = new ApolloClient({
-  // uri: `http://localhost:${process.env.PORT}/graphql`,
+// export const client = new ApolloClient({
+//   // uri: `http://localhost:${process.env.PORT}/graphql`,
+//   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
+//   cache: new InMemoryCache(),
+// });
+
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
-  cache: new InMemoryCache(),
+  fetchOptions: {
+    mode: 'no-cors',
+  },
 });
 
+export const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
