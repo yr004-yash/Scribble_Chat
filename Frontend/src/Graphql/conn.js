43947a6e00eecb6a2c78527e57f999b539e86ce5
Yash <yashsavani004@gmail.com>
2024-02-08 07:41:29 +0530

    Final

--- Frontend/src/Graphql/conn.js
+++ Frontend/src/Graphql/conn.js
@@ -1,21 +1,21 @@
-// import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-// export const client = new ApolloClient({
-//   // uri: `http://localhost:${process.env.PORT}/graphql`,
-//   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
-//   cache: new InMemoryCache(),
-// });
-
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
-const httpLink = createHttpLink({
-  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
-  fetchOptions: {
-    mode: 'no-cors',
-  },
-});
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 export const client = new ApolloClient({
-  link: httpLink,
+  // uri: `http://localhost:${process.env.PORT}/graphql`,
+  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
   cache: new InMemoryCache(),
 });
+
+// import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// const httpLink = createHttpLink({
+//   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
+//   fetchOptions: {
+//     mode: 'no-cors',
+//   },
+// });
+
+// export const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
