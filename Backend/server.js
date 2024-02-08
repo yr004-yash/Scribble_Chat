d602738a8acf1472d9cf03c18f3f70847f935aaa
Yash <yashsavani004@gmail.com>
2024-02-08 11:55:24 +0530

    yes

--- Backend/server.js
+++ Backend/server.js
@@ -68,20 +68,20 @@ app.use(cors({
     optionsSuccessStatus:200,
 }),)
 // app.use(cors());
-app.use(
-	cors({
-		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: function (origin, callback) {
+// 			// allow requests with no origin
+// 			// (like mobile apps or curl requests)
+// 			if (!origin) return callback(null, true);
+// 			if (allowedOrigins.indexOf(origin) === -1) {
+// 				var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+// 				return callback(new Error(msg), false);
+// 			}
+// 			return callback(null, true);
+// 		},
+// 	})
+// );
 app.use(
     '/graphql',
     // cors({ origin: [`${process.env.FRONTEND_URL}`] }),
