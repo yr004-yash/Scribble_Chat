2d90020284fc892bdb438014b2fbadbe9dc62d15
Yash <yashsavani004@gmail.com>
2024-02-12 22:28:26 +0530

    Done

--- Frontend/src/Components/Dashboard/word.jsx
+++ Frontend/src/Components/Dashboard/word.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const Word = ({ sockett }) => {
 
     const { setHiddenWordValue, setusrr } = useHiddenContext();
-    const { hiddenword ,usernm, Lines} = useHiddenContext();
+    const { hiddenword, usernm, Lines } = useHiddenContext();
 
     const [isButtonVisible, setButtonVisibility] = useState(false);
     const [words, setwords] = useState(null);
@@ -18,10 +18,10 @@ const Word = ({ sockett }) => {
     const [timer, settimer] = useState(-1);
     const [restartEmitted, setRestartEmitted] = useState(false);
     const [wordButtonDisabled, setWordButtonDisabled] = useState(false);
-    const [selectedbutton,setselectedbutton]=useState(null);
-    const [check,setcheckk]=useState(false);
-   
-    
+    const [selectedbutton, setselectedbutton] = useState(null);
+    const [check, setcheckk] = useState(false);
+
+
 
     useEffect(() => {
         sockett?.on('Inivisible Button', (space) => {
@@ -78,15 +78,15 @@ const Word = ({ sockett }) => {
 
 
     const handleGenerateWords = () => {
-        if(localStorage.getItem('token')!=null){
+        if (localStorage.getItem('token') != null) {
             var space = "";
             sockett?.emit('Generate words for a random user', space);
         }
-        else{
+        else {
             toast("Oops! Login required.");
         }
     };
-    const handleSelectedWord = (word,number) => {
+    const handleSelectedWord = (word, number) => {
         if (!wordButtonDisabled) {
             sockett?.emit('Selected word', word);
             setWordButtonDisabled(true);
@@ -95,33 +95,39 @@ const Word = ({ sockett }) => {
     };
 
     useEffect(() => {
-        // if(check){
         let interval;
+        let drawingUpdateInterval;
+
         if (timer > 0) {
             interval = setInterval(() => {
-                var timee=timer;
+                var timee = timer;
                 settimer((prevTimer) => prevTimer - 1);
                 sockett?.emit('Update timer', timee - 1);
-                var usr=usernm;
-                if(usr==localStorage.getItem('name')){
+            }, 1000); // Timer interval set to 1000 ms
+
+            drawingUpdateInterval = setInterval(() => {
+                var usr = usernm;
+                if (usr == localStorage.getItem('name')) {
                     sockett?.emit('Updated drawing for backend', Lines);
                 }
-            }, 1000);
-        }
-        else if (timer == 0 && !restartEmitted) {
+            }, 500); // Emit 'Updated drawing for backend' event every 500 ms
+        } else if (timer === 0 && !restartEmitted) {
             clearInterval(interval);
+            clearInterval(drawingUpdateInterval);
+
             var space = "";
-            var usr=usernm;
-            if(usr==localStorage.getItem('name') && hiddenword!=null){
-                sockett?.emit('Display correct word',hiddenword);
+            var usr = usernm;
+            if (usr == localStorage.getItem('name') && hiddenword != null) {
+                sockett?.emit('Display correct word', hiddenword);
             }
             sockett?.emit('Restart all', space);
             setRestartEmitted(true);
         }
 
-        // Clean up the interval on component unmount
-        return () => clearInterval(interval);
-    // }
+        return () => {
+            clearInterval(interval);
+            clearInterval(drawingUpdateInterval);
+        };
     }, [timer, sockett, restartEmitted]);
 
 
@@ -134,71 +140,71 @@ const Word = ({ sockett }) => {
             transition: 'background 0.3s, transform 0.3s',
         }}>
             {/* <div> */}
-                {userr &&
+            {userr &&
                 (
                     <div className='h-1/2 flex justify-content-center'>
-                        <div className='flex justify-content-center align-items-center' style={{width:'20%',marginRight:'0.5rem'}}>
-                            <Avatar email={userr} size={40}/>
+                        <div className='flex justify-content-center align-items-center' style={{ width: '20%', marginRight: '0.5rem' }}>
+                            <Avatar email={userr} size={40} />
                         </div>
-                        <div className='flex flex-col justify-content-center align-items-center' style={{fontSize:'90%'}}>
-                            <i className='h-1/2' style={{fontSize:'90%'}}><b style={{fontSize:'90%'}}>{userr}</b> is drawing...</i>
-                            <div className='h-1/2' style={{fontSize:'90%', color:'#a71313'}}><b>Time remaining : {timer}</b></div>
+                        <div className='flex flex-col justify-content-center align-items-center' style={{ fontSize: '90%' }}>
+                            <i className='h-1/2' style={{ fontSize: '90%' }}><b style={{ fontSize: '90%' }}>{userr}</b> is drawing...</i>
+                            <div className='h-1/2' style={{ fontSize: '90%', color: '#a71313' }}><b>Time remaining : {timer}</b></div>
                         </div>
-                        <div className='flex justify-content-center align-items-center' style={{width:'20%',marginLeft:'0.5rem'}}>
-                            <img src={clock} style={{ height: '100%', width: '100%',marginLeft:'0.5rem' }} alt="clock" />
+                        <div className='flex justify-content-center align-items-center' style={{ width: '20%', marginLeft: '0.5rem' }}>
+                            <img src={clock} style={{ height: '100%', width: '100%', marginLeft: '0.5rem' }} alt="clock" />
                         </div>
                     </div>
                 )
-                }
+            }
             {/* </div> */}
             {isButtonVisible ? (
-                <Button id="generateWordsButton" 
-                        variant="contained" 
-                        onClick={handleGenerateWords} 
-                        className='gradient-button'
-                        style={{
-                            height: '40%',
-                            width: '60%',
-                            background: 'linear-gradient(45deg, #ffffff, #fffffh)',
-                            backgroundSize: 'cover',
-                            borderRadius: '5px',
-                            boxShadow: 'inset 0 0 20px rgba(0, 0, 255, 0.7)',
-                            transition: 'background 0.3s, transform 0.3s',
-                            fontSize:' 0.875rem',
-                            lineHeight: '1.25rem',
-                        }}>
+                <Button id="generateWordsButton"
+                    variant="contained"
+                    onClick={handleGenerateWords}
+                    className='gradient-button'
+                    style={{
+                        height: '40%',
+                        width: '60%',
+                        background: 'linear-gradient(45deg, #ffffff, #fffffh)',
+                        backgroundSize: 'cover',
+                        borderRadius: '5px',
+                        boxShadow: 'inset 0 0 20px rgba(0, 0, 255, 0.7)',
+                        transition: 'background 0.3s, transform 0.3s',
+                        fontSize: ' 0.875rem',
+                        lineHeight: '1.25rem',
+                    }}>
                     Generate Words
                 </Button>
             ) : (
                 words && (
                     <div className='flex-1 align-items-center h-1/2 w-full overflow-x-hidden'>
                         {words.map((word, index) => (
-                            <Button id="generateWordsButton2" 
-                                    key={index} 
-                                    variant="contained" 
-                                    onClick={() => handleSelectedWord(word,index)} 
-                                    disabled={selectedbutton===index?false:wordButtonDisabled} 
-                                    className='word-button'
-                                    style={{
-                                        height: '80%',
-                                        width: '30%',
-                                        margin:'1.5%',
-                                        backgroundSize: 'cover',
-                                        borderRadius: '5px',
-                                        boxShadow: 'inset 0 0 20px rgba(0, 0, 255, 0.6)',
-                                        transition: 'background 0.3s, transform 0.3s',
-                                        transform: selectedbutton===index ? 'scale(1)' : 'scale(0.8)',
-                                        fontSize:' 0.75rem',
-                                        lineHeight: '1rem',
-                                    }}
-                                    >
+                            <Button id="generateWordsButton2"
+                                key={index}
+                                variant="contained"
+                                onClick={() => handleSelectedWord(word, index)}
+                                disabled={selectedbutton === index ? false : wordButtonDisabled}
+                                className='word-button'
+                                style={{
+                                    height: '80%',
+                                    width: '30%',
+                                    margin: '1.5%',
+                                    backgroundSize: 'cover',
+                                    borderRadius: '5px',
+                                    boxShadow: 'inset 0 0 20px rgba(0, 0, 255, 0.6)',
+                                    transition: 'background 0.3s, transform 0.3s',
+                                    transform: selectedbutton === index ? 'scale(1)' : 'scale(0.8)',
+                                    fontSize: ' 0.75rem',
+                                    lineHeight: '1rem',
+                                }}
+                            >
                                 {word}
                             </Button>
                         ))}
                     </div>
                 )
             )}
-            
+
         </div>
     );
 
